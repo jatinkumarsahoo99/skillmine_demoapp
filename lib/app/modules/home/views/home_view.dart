e0c940b6607d75bd83bbda7da1d50e00013b5a99
jatinkumarsahoo <jatnkumarsahoo99@gmail.com>
2024-05-09 15:11:45 +0530

    by jatin

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -13,46 +13,58 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CommonButton(
-              padding: const EdgeInsets.only(left: 24, right: 24, bottom: 16),
-              buttonText: "Call skillmineAuth",
-              onTap: () {
-              Get.to(() => LoginScreen(onLoginResult: (map){
-                Get.snackbar("Response", "Logged in successfully",snackPosition: SnackPosition.BOTTOM );
-                Get.toNamed(Routes.DASHBOARD);
-              }));
-
-            },),
-
-            CommonButton(
-              padding: const EdgeInsets.only(left: 24, right: 24, bottom: 16),
-              buttonText: "SignUp",
-              onTap: () {
-                Get.to(() => RegistrationScreen(
-                  onLogInBtnCallBack: () {
-                    Get.to(() => LoginScreen(onLoginResult: (map){
-                      Get.snackbar("Response", "Logged in successfully",snackPosition: SnackPosition.BOTTOM );
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CommonButton(
+            padding: const EdgeInsets.only(left: 24, right: 24, bottom: 16),
+            buttonText: "Call skillmineAuth",
+            onTap: () {
+              Get.to(() => LoginScreen(
+                    onLoginResult: (map) {
+                      Get.snackbar("Response", "Logged in successfully",
+                          snackPosition: SnackPosition.BOTTOM);
                       Get.toNamed(Routes.DASHBOARD);
-                    }));
-                  },
-                  onSignUpResponse: (Map<dynamic, dynamic> map ) {
-                    Get.to(() => LoginScreen(onLoginResult: (map){
-                      Get.snackbar("Response", "SignUp successfully",snackPosition: SnackPosition.BOTTOM, );
-                      Get.toNamed(Routes.DASHBOARD);
-                    }));
-                  },)
-                );
-                // NavigationServices(context).gotoTabScreen();
-              },
-            ),
-          ],
-        ),
+                    },
+                    clientId: 'c4207198-e277-492a-86c4-6ca74218e18f',
+                  ));
+            },
+          ),
+
+          CommonButton(
+            padding: const EdgeInsets.only(left: 24, right: 24, bottom: 16),
+            buttonText: "SignUp",
+            onTap: () {
+              Get.to(() => RegistrationScreen(
+                    onLogInBtnCallBack: () {
+                      Get.to(() => LoginScreen(
+                            onLoginResult: (map) {
+                              Get.snackbar("Response", "Logged in successfully",
+                                  snackPosition: SnackPosition.BOTTOM);
+                              Get.toNamed(Routes.DASHBOARD);
+                            },
+                            clientId: 'c4207198-e277-492a-86c4-6ca74218e18f',
+                          ));
+                    },
+                    onSignUpResponse: (Map<dynamic, dynamic> map) {
+                      Get.to(() => LoginScreen(
+                            onLoginResult: (map) {
+                              Get.snackbar(
+                                "Response",
+                                "SignUp successfully",
+                                snackPosition: SnackPosition.BOTTOM,
+                              );
+                              Get.toNamed(Routes.DASHBOARD);
+                            },
+                            clientId: 'c4207198-e277-492a-86c4-6ca74218e18f',
+                          ));
+                    },
+                  ));
+              // NavigationServices(context).gotoTabScreen();
+            },
+          ),
+        ],
       ),
     );
   }
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness:Brightness.dark,
+    statusBarBrightness: Brightness.dark,
+    systemNavigationBarDividerColor: Colors.transparent,
+    systemNavigationBarIconBrightness: Brightness.dark,
+  ));
   runApp(
     GetMaterialApp(
       title: "Application",
